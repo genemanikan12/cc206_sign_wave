1ce480fac5c60db92d4cd80fd543974914b7295e
genemanikan12 <eugene.manikan@wvsu.edu.ph>
2023-10-03 19:42:41 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# cc206_sign_wave
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/cc206_sign_wave/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cc206_sign_wave
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
