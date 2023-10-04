a8645d91fe414e7e36d1b67e89b8298a0454b4f8
genemanikan12 <eugene.manikan@wvsu.edu.ph>
2023-10-04 14:49:42 +0800

    exercisefeature

---
+++ lib/features/exercise_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class exercise extends StatelessWidget {
+  const exercise({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          //put widgets here
+        ],
+      ),
+    );
+  }
+}
