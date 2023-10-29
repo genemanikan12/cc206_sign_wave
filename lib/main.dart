ccddaf63180fe6028886fa3e118476be0eb415cb
JulianZapanta <julianroman.zapanta@wvsu.edu.ph>
2023-10-29 18:17:21 +0800

    exercise 5 zapanta villarde

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:cc206_sign_wave/features/sms.dart';
+
 void main() {
   runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  const MainApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Flutter Demo');
-
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: sms(), 
+    );
   }
 }
